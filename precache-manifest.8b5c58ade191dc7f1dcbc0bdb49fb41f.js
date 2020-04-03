6b1fc808559fb46986027feae448068e8dcd3082
Shilpa Soni <shilpassoni@gmail.com>
2020-04-03 15:10:13 +0530

    Updates

---
+++ precache-manifest.8b5c58ade191dc7f1dcbc0bdb49fb41f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b667a7d7b443b396fa3550faa04ab44c",
+    "url": "/learn-todo-list/index.html"
+  },
+  {
+    "revision": "7646248d8edd3fe5d657",
+    "url": "/learn-todo-list/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "a369e8dd0d5210a3ce1f",
+    "url": "/learn-todo-list/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "7646248d8edd3fe5d657",
+    "url": "/learn-todo-list/static/js/2.14ee222a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-todo-list/static/js/2.14ee222a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a369e8dd0d5210a3ce1f",
+    "url": "/learn-todo-list/static/js/main.06be327e.chunk.js"
+  },
+  {
+    "revision": "264b3a525b7f45c33a87",
+    "url": "/learn-todo-list/static/js/runtime-main.0822286f.js"
+  }
+]);
